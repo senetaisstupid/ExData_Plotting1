34e2499c41b333cc4b86bdf76e79ebd068df2ed9
senetaisstupid <senetareston@hotmail.com>
2020-06-10 12:53:43 -0400

    optimized

--- readWeek1Data.R
+++ readWeek1Data.R
@@ -1,14 +1,13 @@
-## Download the data file
-if(!file.exists("household_power_consumption.txt")) {
-    temp <- tempfile()
-    fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"    
-    download.file(fileURL, temp, mode="wb")
-    unzip(temp)
-    unlink(temp)
-}
-
-## Read the conditional data
+## Download and read the data
 if(!file.exists("week1Data.Rda")) {
+    if(!file.exists("household_power_consumption.txt")) {
+        temp <- tempfile()
+        fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"    
+        download.file(fileURL, temp, mode="wb")
+        unzip(temp)
+        unlink(temp)
+    }
+
     library(data.table)
     week1Data <- fread("household_power_consumption.txt", skip="1/2/2007", data.table=FALSE, nrows=2880)
     names(week1Data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", 
